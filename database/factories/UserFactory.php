530f0cc631c4fde49f39117a6ae57c5102df3db7
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-19 17:23:28 -0500

    modification des elements présents dans la table users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,14 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'nom' => fake()->name(),
-            'ville' => fake()->city(),
-            'province' => fake()->state(),
-            'pays' => fake()->country(),
-            'ddn' => fake()->date(),
-            'courriel' => fake()->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'privilege' => $this->faker->randomElement(['Membre', 'Administrateur']),
+            'remember_token' => Str::random(10),
         ];
     }
 
