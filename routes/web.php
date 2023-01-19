42bc449f0115503473857ea2395ea6711421cbf8
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-19 17:26:51 -0500

    test breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\BouteilleController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CellierController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +15,27 @@ use App\Http\Controllers\CellierController;
 |
 */
 
-Route::get('', [CellierController::class, 'index'])->name('celliers.index');
+
+Route::get('/', function () {
+    return view('celliers');
+});
+/* Route::get('', [CellierController::class, 'index'])->name('celliers.index'); */
 
 
 
-Route::get('/updateSAQ', [BouteilleController::class, 'updateSAQ']);
-Route::get('/bouteilles', [BouteilleController::class, 'index'])->name('bouteilles.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Laravel Breeze
+Route::middleware('auth')->group(function () {
+
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+});
+
+require __DIR__ . '/auth.php';
