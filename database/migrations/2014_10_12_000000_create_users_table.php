541fd85d1cd015d20aafc0742d30baae0ab13793
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-01-20 12:11:52 -0500

    Ajout du role par defaut a l'usager lors de la création de son compte

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,16 +17,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nom', 100);
-            $table->string('ville', 50);
-            $table->string('province', 50)->nullable();
             $table->string('pays', 50);
-            $table->date('ddn');
-            $table->string('courriel',100)->unique();
-            $table->string('password',100);
+            $table->string('courriel')->unique();
+            $table->string('password');
             // $table->rememberToken();
-            $table->enum('privilege', ['Membre', 'Administrateur'])->default('Membre');
+            $table->unsignedBigInteger('role_id')->default(2);
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
