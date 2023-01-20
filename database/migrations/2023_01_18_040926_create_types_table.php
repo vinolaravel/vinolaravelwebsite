995d06746fad4d00a640ddc4914cdde0fc9403a6
Mahdi NADIR <e2194498@cmaisonneuve.qc.ca>
2023-01-20 17:05:43 -0500

    ajouter et corriger des migrations

---
+++ database/migrations/2023_01_18_040926_create_types_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        // creation de la table types
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 50);
+            $table->timestamps();
+        });
+
+        /* DB::table('types')->insert([
+            ['type' => 'rouge'],
+            ['type' => 'blanc'],
+        ]); */
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+};
