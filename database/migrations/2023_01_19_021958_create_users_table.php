e7f8deb579baef1e9dec592c52ed1faaffcd6006
Mahdi NADIR <e2194498@cmaisonneuve.qc.ca>
2023-01-20 16:07:57 -0500

    corriger des bugs dans les migrations et definir le bon ordre pour la migration a la BDçà

---
+++ database/migrations/2023_01_19_021958_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 100);
+            $table->string('pays', 50);
+            $table->string('courriel',100)->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('role_id')->default(2);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
