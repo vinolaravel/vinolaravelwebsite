a19589ebc9aae8eaa4e0a074ee9bbb9214c5da56
Mahdi NADIR <mehdinip@gmail.com>
2023-01-23 14:52:59 -0500

    modifier la vue d'un cellier

--- resources/views/uncellier.blade.php
+++ resources/views/uncellier.blade.php
@@ -3,6 +3,7 @@
     @forelse ($bouteilles as $bouteille)
         <div class="bouteille">
             <h1><b>id de la bouteille:</b> {{ $bouteille->id }}</h1>
+            <img src="{{ $bouteille->image }}" alt="{{ $bouteille->nom }}">
             <h2><b>nom de la bouteille:</b> {{ $bouteille->nom }}</h2>
             <h3><b>quantite:</b> {{ $bouteille->quantite }}</h3>
             {{-- <a href="{{ route('bouteilles.show', $bouteille->id) }}">Les détails de la bouteille</a> --}}
@@ -17,17 +18,21 @@
             <button type="submit" class="btn btn-primary">Ajouter</button>
         </form>
 
-        {{-- <form action="{{ route('bouteilles.remove', [$cellier->id, $bouteille->id]) }}" method="post">
+        <form action="{{ route('bouteilles.remove', [$cellier->id, $bouteille->id]) }}" method="post">
             @csrf
+            @method('DELETE')
             <button type="submit" class="btn btn-danger">Enlever</button>
-        </form> --}}
+        </form>
 
         {{-- modifier une bouteille --}}
+        {{-- <a href="{{ route('bouteilles.edit', [$cellier->id, $bouteille->id]) }}">
+            <button class="btn btn-primary">Modifier</button>
+        </a> --}}
     @empty
         <p>Aucune bouteille disponible</p>
-        <a href="">Cliquez ici pour ajouter une bouteille</a>
+        {{-- <a href="">Cliquez ici pour ajouter une bouteille</a> --}}
     @endforelse
 
     <a href="{{ route('celliers') }}">Retour à la liste des celliers</a>
-    {{-- <a href="{{ route('bouteilles.create') }}">Ajouter une bouteille de vin au cellier</a> --}}
+    <a href="{{ route('bouteilles.create', $cellier->id) }}">Ajouter une bouteille de vin au cellier</a>
 </x-app-layout>
