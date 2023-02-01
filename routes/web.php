62ae24292e5edb08622150683bac8aba2ee2b887
Mahdi NADIR <mehdinip@gmail.com>
2023-01-31 22:26:21 -0500

    ajouter des liens pour la plate-forme de l'administration du site

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CellierController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\BouteilleController;
+use App\Http\Controllers\AdminCellierController;
 use App\Http\Controllers\BouteilleSaqController;
+use App\Http\Controllers\AdminBouteilleController;
 use App\Http\Controllers\ChercheBouteilleController;
 
 
@@ -24,25 +28,36 @@ Route::middleware('auth')->group(function () {
     Route::get('/celliers/{id}/edit', [CellierController::class, 'edit'])->name('celliers.edit');
     Route::put('/celliers/{id}', [CellierController::class, 'update'])->name('celliers.update');
     Route::delete('/celliers/{id}', [CellierController::class, 'destroy'])->name('celliers.destroy');
-
-
     Route::get('/celliers/{id}/bouteilles', [CellierController::class, 'show'])->name('celliers.show');
-
-    // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     Route::get('/celliers/{id}/bouteilles/{idBouteille}/edit', [BouteilleController::class, 'edit'])->name('bouteilles.edit');
     Route::put('/celliers/{id}/bouteilles/{idBouteille}/update', [BouteilleController::class, 'update'])->name('bouteilles.update');
-    // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
     Route::delete('/celliers/{id}/bouteilles/{idBouteille}', [BouteilleController::class, 'destroy'])->name('bouteilles.remove');
     Route::get('/celliers/{id}/bouteilles/createBouteille', [BouteilleController::class, 'create'])->name('bouteilles.create');
     Route::post('/celliers/{id}/bouteilles/storeBouteille', [BouteilleController::class, 'store'])->name('bouteilles.store');
     Route::get('/celliers/{id}/bouteilles/{idbouteille}', [CellierController::class, 'detailsBouteille'])->name('celliers.detailsBouteille');
     Route::post('/celliers/{id}/bouteilles/add/{idBouteille}', [BouteilleController::class, 'addBouteille'])->name('bouteilles.add');
     Route::post('/celliers/{id}/bouteilles/drink/{idBouteille}', [BouteilleController::class, 'drinkBouteille'])->name('bouteilles.drink');
+
+
+
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+    Route::get('/admin/celliers', [AdminCellierController::class, 'index'])->name('admin.celliers');
+    Route::get('/admin/bouteilles', [AdminBouteilleController::class, 'index'])->name('admin.bouteilles');
+    Route::get('/admin/users', [AdminUserController::class, 'index'])->name('admin.users');
+    Route::get('/admin/users/{idUser}', [AdminCellierController::class, 'show'])->name('admin.afficheCelliers');
+    Route::delete('/admin/users/{idUser}', [AdminUserController::class, 'destroy'])->name('admin.userDelete');
+    Route::get('/admin/users/{idUser}/{idCellier}/change', [AdminCellierController::class, 'edit'])->name('admin.editCellier');
+    Route::put('/admin/users/{idUser}/{idCellier}', [AdminCellierController::class, 'update'])->name('admin.updateCellier');
+    Route::delete('/admin/users/{idUser}/{idCellier}', [AdminCellierController::class, 'destroy'])->name('admin.cellierDelete');
+
+
+
     Route::get('/updateSAQ', [BouteilleSaqController::class, 'updateSAQ'])->name('updateSAQ');
 
-    // link for search
+
+
     Route::get('/search', [ChercheBouteilleController::class, 'search'])->name('search');
+
 
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
