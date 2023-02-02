0c251b44a2c394a4d8f4898d4ec9d31c60accc2e
Mahdi NADIR <mehdinip@gmail.com>
2023-02-01 20:19:41 -0500

    ajouter le lien et le script de pagination de bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,14 @@
 
         <!-- CSS-->
         @if (auth()->user()->role_id != 1)
+            
             <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}">
         @endif
+        
+        {{-- bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script differ src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
