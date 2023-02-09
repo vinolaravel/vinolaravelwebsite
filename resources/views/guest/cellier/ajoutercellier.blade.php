d0659ea16fec164a4b884f7348836dadea0b4de3
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-02-08 22:59:48 -0500

    modification des noms de classe et des balises html de la page ajout cellier

--- resources/views/guest/cellier/ajoutercellier.blade.php
+++ resources/views/guest/cellier/ajoutercellier.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
-    <section class="form">
-        <h5 class="form-title">Ajouter un cellier</h5><br>
+    <p class="titre">Ajouter un cellier</p>
+    <section class="formAjoutCellier">
         <form action="{{ route('celliers.store', auth()->user()->id) }}" method="post">
             @csrf
-            <div class="form-group">
-                <label for="nom">Nom du cellier</label>
+            <div class="divAjoutCellier">
+                <label  for="nom">Nom du cellier</label>
                 <input type="text" name="nom" id="nom" placeholder="Entrez le nom du cellier">
             </div>
 
-            <button type="submit">Ajouter</button>
+            <button class="btnRouge" type="submit">Ajouter</button>
         </form>
     </section>
 </x-app-layout>
