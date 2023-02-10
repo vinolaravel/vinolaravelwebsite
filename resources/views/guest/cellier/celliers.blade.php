7953ed81c6e8c8acead7f652fc5c3a5a5aa1452e
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-02-09 21:35:47 -0500

    ajout de la boite modal pour la suppression de cellier

--- resources/views/guest/cellier/celliers.blade.php
+++ resources/views/guest/cellier/celliers.blade.php
@@ -6,7 +6,7 @@
         <div class="cellier">
             <div class="flexImgMapNomCellier">
                 <img src="/img/map.png" alt="map">
-                <span class="nomCellier">{{ $cellier->nom }}</span>
+                <span data-js-nom-cellier class="nomCellier">{{ $cellier->nom }}</span>
             </div>
             @if ($cellier->nbBouteilles == 0)
                 <p>Ce cellier est vide!</p>                
@@ -32,7 +32,7 @@
                     <form action="{{ route('celliers.destroy', $cellier->id) }}" method="post">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btnRouge btnSupp">Supprimer</button>
+                        <button data-js-btn-supp class="btnRouge btnSupp">Supprimer</button>
                     </form>
                 </div>
             </div>
@@ -42,5 +42,51 @@
             <p class="paraAucunCellier">Aucun cellier disponible</p>
         </div>
         @endforelse
-            <!-- <a href="{{ route('celliers.create', auth()->user()->id) }}">Créer un nouveau cellier</a> -->
-</x-app-layout>
+
+
+        </x-app-layout>
+        <div class="modal-overl">
+            <div class="modal-contenu">
+                <div>
+                    <h4>Supprimer le cellier <span data-js-nom-cellier-modal></span></h4>
+                </div>
+                <div>
+                    <p>Etes-vous sûr ?</p>
+                </div>
+                <div class="modal-buttons">
+                    <button class="yes">Oui</button>
+                    <button class="no">Non</button>
+                </div>
+            </div>
+        </div>
+
+
+        <script>
+
+            let celliers = document.querySelectorAll('.cellier');
+            for (let i = 0; i < celliers.length; i++) {
+                let cellierForm = celliers[i].querySelector('form');
+                let btnSupp = celliers[i].querySelector('.btnSupp');
+
+                btnSupp.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    let modal = document.querySelector('.modal-overl');
+                    let nomCellier = document.querySelectorAll('[data-js-nom-cellier]')[i].textContent;
+                    let nomCellierModal = document.querySelector('[data-js-nom-cellier-modal]');
+                    nomCellierModal.textContent = nomCellier;
+                    // display flex for modal
+                    modal.style.display = 'flex';
+                    
+                    let btnYes = document.querySelector('.yes');
+                    btnYes.addEventListener('click', function () {
+                        cellierForm.submit();
+                    });
+
+                    let btnNo = document.querySelector('.no');
+                    btnNo.addEventListener('click', function () {
+                        modal.style.display = 'none';
+                    });
+                });
+            }
+            
+        </script>
